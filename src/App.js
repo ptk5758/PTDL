4ecc4ce401ee4ec42c597835db84b7afc2588d36
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2023-01-17 00:47:15 +0900

    front 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const tempData = [
+    {
+      isComplete : false,
+      text : "내용이다."
+    },
+    {
+      isComplete : false,
+      text : "내용이다."
+    },
+  ]
+  const [ list, setList ] = useState(tempData);
   return (
     <div className='content'>
       <div className='title'>
@@ -12,41 +24,27 @@ function App() {
           <div className='middle'><p>Content</p></div>
           <div className='right'><p>option</p></div>
         </div>
-        <div className='list'>
-          <div className='left'><p className='chk-box'>&nbsp;</p></div>
-          <div className='middle'><input className='insert'/></div>
-          <div className='right'>
-            <p>수정</p>
-            <p>삭제</p>
-          </div>
-        </div>
-        <div className='list'>
-          <div className='left'><p className='chk-box'>&nbsp;</p></div>
-          <div className='middle'><input className='insert'/></div>
-          <div className='right'>
-            <p>수정</p>
-            <p>삭제</p>
-          </div>
-        </div>
-        <div className='list'>
-          <div className='left'><p className='chk-box'>&nbsp;</p></div>
-          <div className='middle'><input className='insert'/></div>
-          <div className='right'>
-            <p>수정</p>
-            <p>삭제</p>
-          </div>
-        </div>
-        <div className='list'>
-          <div className='left'><p className='chk-box'>&nbsp;</p></div>
-          <div className='middle'><input className='insert'/></div>
-          <div className='right'>
-            <p>수정</p>
-            <p>삭제</p>
-          </div>
-        </div>
+        { list && list.map((obj, index)=>{
+          return <List key={index} list={obj} />
+        }) }      
         <div className='list'>
           <p> 항목 추가하기 </p>
         </div>
+      </div>
+    </div>
+  );
+}
+
+function List(props) {
+  const list = props.list;
+  console.log(list);
+  return(
+    <div className='list'>
+      <div className='left'><p className='chk-box'>&nbsp;</p></div>
+      <div className='middle'><input className='insert'/></div>
+      <div className='right'>
+        <p>수정</p>
+        <p>삭제</p>
       </div>
     </div>
   );
