27d286976287ae286b0c4084a1d094c9fd10bc58
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2023-01-16 23:59:28 +0900

    css sace

--- src/App.js
+++ src/App.js
@@ -2,8 +2,33 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      PTK To Do List
+    <div className='content'>
+      <div className='title'>
+        <p>To Do List</p>
+      </div>
+      <div className='board'>
+        <div className='head'>
+          <div className='left'>
+            <p>Checked</p>
+          </div>
+          <div className='right'>
+            <p>Content</p>
+            <p>Option</p>
+          </div>
+        </div>
+        <div className='list'>
+          <div className='left'>
+            <p>ㅁ</p>
+          </div>
+          <div className='right'>
+            <p>ㅡㅡ</p>
+            <div className='opt'>
+              <p>수정</p>
+              <p>삭제</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
