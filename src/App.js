87ae33ce0f6d338f8fc4b5250222cabbdc05edfd
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2023-01-18 23:31:04 +0900

    주석 추가

--- src/App.js
+++ src/App.js
@@ -3,52 +3,51 @@ import './App.css';
 import checkIcon from './source/img/check-icon.png';
 
 function App() {
-  const tempData = [
-    {
-      isComplete : false,
-      text : "1"
-    },
-    {
-      isComplete : false,
-      text : "2"
-    },
-    {
-      isComplete : false,
-      text : "3"
-    },
-    {
-      isComplete : false,
-      text : "4"
-    },
-  ]
-  const [ list, setList ] = useState([]);
+  // 항목을 관리하는 State Object[]
+  const [ list, setList ] = useState([]); 
+
+  // 항목을 추가하는 함수
   function addList(){
-    const temp = {
-      isComplete : false,
-      text : ""
+    const temp = { // list 안에 설계해둔 Object가 들어가기때문에 temp 오브젝트 생성
+      isComplete : false, // 항목이 check 가 되있는지 안되있는지
+      text : "" // 항목의 텍스트 (contents)
     }
     setList([...list, temp]);
   }
+  
+  /**
+   * 삭제하려는 항목의 index를 받아 list 변수에 접근하여    
+   * 해당 Index 에 항복 삭제
+   */
   function removeItem(index) {    
     const temp = [...list];
     temp.splice(index,1);
-    console.log(temp);
+    // console.log(temp);
     setList(temp);    
   }
 
+  /**
+   * 항목 체크박스 기능 함수
+   * 선택한 항목의 인덱스를 가져와서
+   * 해당 list의 인덱스에 접근해서 object를 갈아끼우기?   
+   */
   function checkToggle(index) {    
-    const tlist = [...list];
+    const tlist = [...list]; // type Object[]
     const otemp = { ...tlist[index], isComplete : !tlist[index].isComplete };
-    tlist[index] = otemp;
+    tlist[index] = otemp; // 수정한 Object 객체를 tlist 에넣고 state변수 교체
     setList(tlist);
   }
 
+  /**
+   * 항목의 텍스트를 수정할때 함수
+   */
   function valueChange(e, index) {    
     const tlist = [...list];
     const otemp = { ...tlist[index], text : e.target.value };    
     tlist[index] = otemp;
     setList(tlist);
   }
+
   return (
     <div className='content'>
       <div className='title'>
@@ -58,7 +57,7 @@ function App() {
         <div className='head'>
           <div className='left'><p>Checked</p></div>
           <div className='middle'><p>Content</p></div>
-          <div className='right'><p>option</p></div>
+          <div className='right'><p>option</p></div>          
         </div>
         { list && list.map((obj, index)=>{
           return <List key={index} list={obj} remove={removeItem} index={index} checkToggle={checkToggle} valueChange={valueChange} />
